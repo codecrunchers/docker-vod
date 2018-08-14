d64d8b35ed82e08f98f804f1a5e97b6ba3231903
Alan <alan@codecrunchers.ie>
2018-08-14 08:53:34 +0100

    working!

--- discovery-service/entrypoint.sh
+++ discovery-service/entrypoint.sh
@@ -3,5 +3,5 @@ set -e
 lock="/var/run/kvms"
 exec 200>$lock
 flock -n 200 || exit 1
-/usr/bin/nmap -sP $CIDR | awk -v mac="$MAC_ID" '/report for/{r=$5}/MAC Address/{p=$3}$0~mac{print r,p}' > "/data/cameras.txt"
+/usr/bin/nmap -sP $CIDR | awk -v mac="$MAC_ID" '/report for/{r=$5}/MAC Address/{p=$3}$0~mac{print r,p}' >> "/data/cameras.txt"
 
--- rtsp-client/entrypoint.sh
+++ rtsp-client/entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
-set -x
 lock="/var/run/kvms"
 exec 200>$lock
 flock -n 200 || exit 1
-IP=$(tail  /data/cameras.txt | cut -d' ' -f 1) \
-MAC_ID=$(tail  /data/cameras.txt | cut -d' ' -f 2) \
+#if [ -s /data/cameras.txt ]; then
+IP=$(tail  /data/cameras.txt | cut -d' ' -f 1)
+MAC_ID=$(tail  /data/cameras.txt | cut -d' ' -f 2)
 MAC_ID=$(echo "$MAC_ID" | sed 's/\:/_/g')
+sed -i '$ d' /data/cameras.txt
 export IP MAC_ID
 echo "Connecting to $IP/$MAC_ID"
 ffmpeg -i "rtsp://$IP:8554/unicast" -stats -an -vcodec copy -f flv -s 32x32 -rtmp_live recorded "rtmp://media-server/hlspub/$MAC_ID"
-
-
+#fi
