969739e81a57a75873f5a43ff34300647ea97450
Alan <alan@codecrunchers.ie>
2018-09-02 03:43:48 +0100

    add some details

--- nginx-vms/scripts/run_app.sh
+++ nginx-vms/scripts/run_app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-/bin/echo \"{\"event\":\"mp4_create\",\"value\":\"$1\"}\" >> /home/nginx/logs/vms.log
-exec "$1" &
+/bin/echo \"{\"event\":\"mp4_create\",\"value\":\"\$1\"}\" >> /home/nginx/logs/vms.log
+#$("$1")
 
 
 
